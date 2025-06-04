2d625d03d1937974c763bdc26fa39b5091b0bb66
SwikritiDhakal <151549393+SwikritiDhakal@users.noreply.github.com>
2025-06-04 19:19:20 +0545

    handling form submission

--- ReactProject/src/components/Form.jsx
+++ ReactProject/src/components/Form.jsx
@@ -4,17 +4,25 @@ export default function Form(){
 
     const [name, setName]=useState({firstname:"", lastname:""});
 
-    function handleChange(e){
-       setName(e.target.value);
-    }
+
+      function handleSubmit(e){
+         e.preventDefault();
+         console.log(name);
+      }
+
+   //  function handleChange(e){
+   //     setName(e.target.value);
+   //  }
     return (
     <div>
+      {name.firstname}-{name.lastname}
+      <form action="">
         <input type="text" 
         // onChange={function demo(e){
         //     return handleChange(e);
         // } }
        // onChange={(e)=>handleChange(e)}
-       onChange={(e)=>setName({firstname:e.target.value, lastname:""})}
+       onChange={(e)=>setName({...name,firstname:e.target.value})}
 
           value={name.firstname}/>
         <input type="text" 
@@ -22,9 +30,12 @@ export default function Form(){
         //     return handleChange(e);
         // } }
        // onChange={(e)=>handleChange(e)}
-       onChange={(e)=>setName({firstname:"" ,lastname:e.target.value})}
+       onChange={(e)=>setName({...name,lastname:e.target.value})}
 
           value={name.lastname}/>
-    </div>
+
+          <button onClick={(e)=>handleSubmit(e)}>Add</button>
+          </form>
+    </div> 
     );
 }
